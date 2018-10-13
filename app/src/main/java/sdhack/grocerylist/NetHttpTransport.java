87897c397237d96cca2f32d9eddc33dfabfee351
AjayBoot <44077602+AjayBoot@users.noreply.github.com>
2018-10-13 11:32:54 -0700

    Cloud API add-ons

--- app/src/main/java/sdhack/grocerylist/NetHttpTransport.java
+++
@@ -1,4 +0,0 @@
-package sdhack.grocerylist;
-
-class NetHttpTransport {
-}
--- app/src/main/java/sdhack/grocerylist/VisionIntentService.java
+++ app/src/main/java/sdhack/grocerylist/VisionIntentService.java
@@ -10,14 +10,5 @@ public class VisionIntentService implements IntentService {
 
     @Override
     protected void onHandleIntent(@androidx.annotation.Nullable Intent intent) {
-
-        VisionIntentService visionBuilder = new VisionIntentService(
-                new NetHttpTransport(),
-                new AndroidJsonFactory(),
-                null);
-
-        new VisionIntentService("AIzaSyBjck0TMRVFDG0AxoI5Gh1n1EmRr7jRZG0", netHttpTransport, AndroidJsonFactory androidJsonFactory, Object o);
-        visionBuilder.setVisionRequestInitializer();
-        );
     }
 }

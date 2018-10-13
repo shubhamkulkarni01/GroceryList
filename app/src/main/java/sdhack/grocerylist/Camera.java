233356ee33ed35b5ef47f998aaf6553f7467abeb
AjayBoot <44077602+AjayBoot@users.noreply.github.com>
2018-10-13 13:51:03 -0700

    Cloud API add-ons

--- app/src/main/java/sdhack/grocerylist/Camera.java
+++ app/src/main/java/sdhack/grocerylist/Camera.java
@@ -1,56 +1,10 @@
 package sdhack.grocerylist;
 
-import android.graphics.SurfaceTexture;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraCaptureSession;
-import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraManager;
-import android.hardware.camera2.CameraMetadata;
-import android.hardware.camera2.params.StreamConfigurationMap;
-import android.util.Size;
-
 public class Camera {
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    CameraManager cameraManager;
-    Size size;
-    String cameraId;
-    cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
-
-    try {
-        String[] cameraIds = cameraManager.getCameraIdList();
-
-        for(String cameraId : cameraIds) {
-            CameraCharacteristics cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraId);
-
-            if(cameraCharacteristics.get(cameraCharacteristics.LENS_FACING) == CameraMetadata.LENS_FACING_FRONT) {
-                this.cameraId = cameraId;
-                StreamConfigurationMap streamConfigurationMap = cameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
-                size = streamConfigurationMap.getOutputSizes(SurfaceTexture.class) [0];
-
-            }
-    }
-    catch(CameraAccessException e) {
-        e.printStackTrace();
-    }
-
+    
 
     }

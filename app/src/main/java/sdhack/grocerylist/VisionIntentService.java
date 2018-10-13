9078f5cf23e8d9e5e962eeed2d7bc54e8ef766e1
AjayBoot <44077602+AjayBoot@users.noreply.github.com>
2018-10-13 12:25:37 -0700

    Cloud API add-ons

--- app/src/main/java/sdhack/grocerylist/VisionIntentService.java
+++ app/src/main/java/sdhack/grocerylist/VisionIntentService.java
@@ -4,8 +4,11 @@ import android.app.IntentService;
 import android.content.Intent;
 
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -22,15 +25,33 @@ public class VisionIntentService extends IntentService {
     @Override
     protected void onHandleIntent( Intent intent) {
 
-        URL serverUrl = new URL(TARGET_URL + API_KEY);
-        URLConnection urlConnection = serverUrl.openConnection();
+        URL serverUrl = null;
+        try {
+            serverUrl = new URL(TARGET_URL + API_KEY);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        URLConnection urlConnection = null;
+        try {
+            urlConnection = serverUrl.openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         HttpURLConnection httpConnection = (HttpURLConnection)urlConnection;
 
-        httpConnection.setRequestMethod("POST");
+        try {
+            httpConnection.setRequestMethod("POST");
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        }
         httpConnection.setRequestProperty("Content-Type", "application/json");
         httpConnection.setDoOutput(true);
 
-        String response = httpConnection.getResponseMessage();
+        try {
+            String response = httpConnection.getResponseMessage();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }

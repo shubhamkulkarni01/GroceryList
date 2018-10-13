8566e43e0eec91f3cb5ea78db0f22f0b109cc9cd
AjayBoot <44077602+AjayBoot@users.noreply.github.com>
2018-10-13 12:29:57 -0700

    Cloud API add-ons

--- app/src/main/java/sdhack/grocerylist/VisionIntentService.java
+++ app/src/main/java/sdhack/grocerylist/VisionIntentService.java
@@ -11,6 +11,7 @@ import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Scanner;
 
 public class VisionIntentService extends IntentService {
     public VisionIntentService(String name) {
@@ -47,11 +48,54 @@ public class VisionIntentService extends IntentService {
         httpConnection.setRequestProperty("Content-Type", "application/json");
         httpConnection.setDoOutput(true);
 
+        BufferedWriter httpRequestBodyWriter = null;
+        try {
+            httpRequestBodyWriter = new BufferedWriter(new
+                    OutputStreamWriter(httpConnection.getOutputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            httpRequestBodyWriter.write
+                    ("{\"requests\":  [{ \"features\":  [ {\"type\": \"LABEL_DETECTION\""
+                            +"}], \"image\": {\"source\": { \"gcsImageUri\":"
+                            +" \"gs://vision-sample-images/4_Kittens.jpg\"}}}]}");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            httpRequestBodyWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         try {
             String response = httpConnection.getResponseMessage();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        try {
+            if (httpConnection.getInputStream() == null) {
+                System.out.println("No stream");
+                return;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Scanner httpResponseScanner = null;
+        try {
+            httpResponseScanner = new Scanner(httpConnection.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String resp = "";
+        while (httpResponseScanner.hasNext()) {
+            String line = httpResponseScanner.nextLine();
+            resp += line;
+            System.out.println(line);  //  alternatively, print the line of response
+        }
+        httpResponseScanner.close();
     }
 }

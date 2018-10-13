81878469a9632aec9fdc664a982223d61a65e31b
shubhamkulkarni01 <shubhamkulkarni01@gmail.com>
2018-10-13 13:12:16 -0700

    hello world

--- app/src/main/java/sdhack/grocerylist/MainActivity.java
+++ app/src/main/java/sdhack/grocerylist/MainActivity.java
@@ -1,10 +1,12 @@
 package sdhack.grocerylist;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,12 +14,21 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mRecyclerView = findViewById(R.id.my_recycler_view);
+        fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }});
 
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView

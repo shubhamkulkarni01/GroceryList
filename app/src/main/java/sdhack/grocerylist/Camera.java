b5d30a5833520086dac1335fe764c47ddb6f651e
AjayBoot <44077602+AjayBoot@users.noreply.github.com>
2018-10-13 14:21:24 -0700

    Cloud API add-ons

--- app/src/main/java/sdhack/grocerylist/Camera.java
+++ app/src/main/java/sdhack/grocerylist/Camera.java
@@ -1,10 +1,47 @@
 package sdhack.grocerylist;
 
-public class Camera {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+public class Camera extends AppCompatActivity {
 
+    private CameraKitView cameraKitView;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        cameraKitView = findViewById(R.id.camera);
+    }
 
-    
+    @Override
+    protected void onStart() {
+        super.onStart();
+        cameraKitView.onStart();
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraKitView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        cameraKitView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        cameraKitView.onStop();
+        super.onStop();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        cameraKitView.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+       
     }

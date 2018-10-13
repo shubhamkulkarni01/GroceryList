6f17b1ed9617dea6ca7081ad8f8e4035600e6a94
shubhamkulkarni01 <shubhamkulkarni01@gmail.com>
2018-10-13 13:30:33 -0700

    hello world

--- app/src/main/java/sdhack/grocerylist/MyAdapter.java
+++ app/src/main/java/sdhack/grocerylist/MyAdapter.java
@@ -2,20 +2,41 @@ package sdhack.grocerylist;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
-public class MyAdapter extends RecyclerView.Adapter {
+public class MyAdapter extends RecyclerView.Adapter <MyAdapter.MyViewHolder> {
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            itemView.findViewById()
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return super.equals(obj);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString();
+        }
+    }
 
     GroceryList groceryList;
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout., viewGroup, false);
+        return new MyViewHolder(view);;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull MyViewHolder viewHolder, int i) {
 
     }
 

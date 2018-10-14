09d16eaa5a66c6200ed4c30b4c86b96ca7e4935a
shubhamkulkarni01 <shubhamkulkarni01@gmail.com>
2018-10-13 17:17:15 -0700

    hello world

--- app/src/main/java/sdhack/grocerylist/Camera.java
+++ app/src/main/java/sdhack/grocerylist/Camera.java
@@ -1,8 +1,11 @@
 package sdhack.grocerylist;
+
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 
-import com.camerakit.*;
+import com.camerakit.CameraKit;
+import com.camerakit.CameraKitView;
 
 public class Camera extends AppCompatActivity {
 
@@ -13,6 +16,8 @@ public class Camera extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
         cameraKitView = findViewById(R.id.camera);
+        cameraKitView.setFocus(CameraKit.FOCUS_CONTINUOUS);
+
     }
 
         @Override
@@ -40,8 +45,7 @@ public class Camera extends AppCompatActivity {
         }
 
         @Override
-        public void onRequestPermissionsResult ( int requestCode, String[] permissions,
-        int[] grantResults){
+        public void onRequestPermissionsResult (int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
             super.onRequestPermissionsResult(requestCode, permissions, grantResults);
             cameraKitView.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }

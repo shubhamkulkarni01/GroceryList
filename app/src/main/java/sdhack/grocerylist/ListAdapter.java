60a41b2676391b53d96a33f4a8e02be62192fd13
shubhamkulkarni01 <shubhamkulkarni01@gmail.com>
2018-10-13 20:16:15 -0700

    hello world

--- app/src/main/java/sdhack/grocerylist/ListAdapter.java
+++ app/src/main/java/sdhack/grocerylist/ListAdapter.java
@@ -2,22 +2,40 @@ package sdhack.grocerylist;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
-public class ListAdapter extends RecyclerView.Adapter {
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ListViewHolder> {
+    GroceryList list;
+
+    public ListAdapter(GroceryList list) {
+        super();
+        this.list = list;
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public ListViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        ListViewHolder holder = new ListViewHolder(LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.grocery_list_detail, viewGroup, false));
+        return holder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull ListViewHolder viewHolder, int i) {
 
     }
+    class ListViewHolder extends RecyclerView.ViewHolder{
 
-    public ListAdapter(String name) {
-        super(name);
+        public ListViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+        }
     }
 }
 
